d96f35dda79a06afc8f6b00c30790bcbb1c0b368
amu94777 <amu94777@gmail.com>
2024-02-01 15:27:27 +0530

    vpc-test

--- main.tf
+++ main.tf
@@ -3,5 +3,7 @@ module "roboshop" {
     project_name = var.project_name
     environment = var.environment
     cidr_block = var.cidr_block
-    
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
 }
---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_info" {
+    value = module.roboshop.vpc_info
+}
+output "vpc_id" {
+    value = module.roboshop.vpc_id
+}
+output "azs" {
+    value = module.roboshop.azs
+}
